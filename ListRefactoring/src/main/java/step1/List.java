68a311d9acf03d42bee92f5906098f34dfc760fc
SK 테크엑스 권성영 <kkwonsy@sk.com>
2019-04-25 15:19:00 +0900

    -list refactoring #1 -- cmd+opt+C -- opt+enter -- cmd+opt+M

--- ListRefactoring/src/main/java/step1/List.java
+++ ListRefactoring/src/main/java/step1/List.java
@@ -1,26 +1,43 @@
 package step1;
 
+import java.util.Comparator;
+
 public class List {
-	private Object[] elements = new Object[10];
+
+	public static final int INITIAL_STORE_SIZE = 10;
+	public static final int STORE_SIZE_ICREMENT = 10;
+	private Object[] elements = new Object[INITIAL_STORE_SIZE];
 	private boolean readOnly;
 	private int size = 0;
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			
-			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + 10];
-				for (int i = 0; i < size; i++) {
-					newElements[i] = elements[i];
-				}
-
-				elements = newElements;
-			}
-
-			elements[size] = element;
-			size++;
+		if (readOnly) {
+			return;
 		}
+
+		if (isElementSotreFull()) {
+			increaseElementStore();
+		}
+
+		addElementAtEnd(element);
+	}
+
+	private void increaseElementStore() {
+		Object[] newElements = new Object[elements.length + STORE_SIZE_ICREMENT];
+		for (int i = 0; i < size; i++) {
+			newElements[i] = elements[i];
+		}
+
+		elements = newElements;
+	}
+
+	private boolean isElementSotreFull() {
+		return size + 1 > elements.length;
+	}
+
+	private void addElementAtEnd(Object element) {
+		elements[size] = element;
+		size++;
 	}
 
 	public int size() {
